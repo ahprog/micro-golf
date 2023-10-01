36c2c53da151d77d78029b59fe4890f19a7aa201
Antonin Herrero <herrero.antonin@gmail.com>
2023-10-01 18:10:00 +0200

    made some titles

--- LevelItem.gd
+++ LevelItem.gd
@@ -2,6 +2,25 @@ extends Button
 
 signal level_selected(num)
 
+var icons: Array = [
+	preload("res://sprites/level_icon/chiffre1.png"),
+	preload("res://sprites/level_icon/chiffre2.png"),
+	preload("res://sprites/level_icon/chiffre3.png"),
+	preload("res://sprites/level_icon/chiffre4.png"),
+	preload("res://sprites/level_icon/chiffre5.png"),
+	preload("res://sprites/level_icon/chiffre6.png"),
+	preload("res://sprites/level_icon/chiffre7.png"),
+	preload("res://sprites/level_icon/chiffre8.png")
+	]
+var icon2 = preload("res://sprites/level_icon/chiffre2.png")
+var icon3 = preload("res://sprites/level_icon/chiffre3.png")
+var icon4 = preload("res://sprites/level_icon/chiffre4.png")
+var icon5 = preload("res://sprites/level_icon/chiffre5.png")
+var icon6 = preload("res://sprites/level_icon/chiffre6.png")
+var icon7 = preload("res://sprites/level_icon/chiffre7.png")
+var icon8 = preload("res://sprites/level_icon/chiffre8.png")
+
+
 var level_num: int
 var locked: bool = true
 
@@ -10,8 +29,8 @@ func _ready():
 	pass # Replace with function body.
 
 func set_level_num(num: int):
+	icon = icons[num - 1]
 	locked = false
-	text = str(num)
 	level_num = num
 
 func _on_LevelItem_pressed():
